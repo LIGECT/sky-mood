1cec320167db57d2a724bfc3ce2cd6cc000a3766
LIGECT <viminiko@gmail.com>
2026-02-02 23:19:05 +0200

    Configure Webpack

---
+++ webpack.dev.js
@@ -0,0 +1,27 @@
+import path from 'path';
+import { merge } from 'webpack-merge';
+import common from './webpack.common.js';
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+export default merge(common, {
+  mode: 'development',
+  devtool: 'eval-source-map',
+  devServer: {
+    static: { directory: path.resolve(__dirname, 'dist') },
+    hot: true,
+    open: true,
+    watch: true,
+    watchFiles: ['src/**/*.html'],
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader'],
+      },
+    ],
+  },
+});
---
+++ webpack.prod.js
@@ -0,0 +1,51 @@
+import { merge } from 'webpack-merge';
+import common from './webpack.common.js';
+import MiniCssExtractPlugin from 'mini-css-extract-plugin';
+import CompressionPlugin from 'compression-webpack-plugin';
+import { BundleAnalyzerPlugin } from 'webpack-bundle-analyzer';
+
+const prodConfig = {
+  mode: 'production',
+  devtool: false,
+
+  plugins: [new CompressionPlugin()],
+
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: [MiniCssExtractPlugin.loader, 'css-loader', 'postcss-loader'],
+      },
+    ],
+  },
+
+  optimization: {
+    splitChunks: {
+      chunks: 'all',
+      minSize: 20000,
+      minRemainingSize: 0,
+      minChunks: 1,
+      maxAsyncRequests: 30,
+      maxInitialRequests: 30,
+      enforceSizeThreshold: 50000,
+      cacheGroups: {
+        defaultVendors: {
+          test: /[\\/]node_modules[\\/]/,
+          priority: -10,
+          reuseExistingChunk: true,
+        },
+        default: {
+          minChunks: 2,
+          priority: -20,
+          reuseExistingChunk: true,
+        },
+      },
+    },
+  },
+};
+
+if (process.env.ANALYZE) {
+  prodConfig.plugins.push(new BundleAnalyzerPlugin({ analyzerMode: 'server' }));
+}
+
+export default merge(common, prodConfig);
