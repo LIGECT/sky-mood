47238387d01bb9fbf61912010836afeaa0bbe1b6
LIGECT <viminiko@gmail.com>
2026-02-16 21:39:48 +0200

    feat(ui): Wire search form to weather fetch

--- src/index.js
+++ src/index.js
@@ -1,25 +1,40 @@
 import './style.css';
-import { processWeatherData } from './weatherData.js'
+import { processWeatherData } from './weatherData.js';
+
+const form = document.getElementById('search');
+const input = document.getElementById('city-input');
 
 async function getWeather(city) {
-	try{
-		const api = process.env.API_KEY;
+  try {
+    const api = process.env.API_KEY;
 
-		const url  = new URL(`https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${city}?key=${api}`)
-		
-		const response = await fetch(url)
+    const url = new URL(
+      `https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${city}?key=${api}`,
+    );
 
-		if(!response.ok){
-			throw Error(`Ooops... Error network: ${response.status} ${response.statusText}`)
-		}
+    const response = await fetch(url);
 
-		const data = await response.json()
-		const processData = processWeatherData(data)
-		console.log(processData)
+    if (!response.ok) {
+      throw Error(
+        `Ooops... Error network: ${response.status} ${response.statusText}`,
+      );
+    }
 
-	} catch (error) {
-		console.error("Damn, error:", error)
-	}
+    const data = await response.json();
+    const processData = processWeatherData(data);
+    console.log(processData);
+  } catch (error) {
+    console.error('Damn, error:', error);
+  }
 }
 
-getWeather('London')
+form.addEventListener('submit', (e) => {
+  e.preventDefault();
+  const cityValue = input.value.trim();
+  if (cityValue === '') {
+    console.warn('Enter city');
+    return;
+  } else {
+    getWeather(cityValue);
+  }
+});
